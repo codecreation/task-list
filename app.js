595d4b38111cb0efe72bd80bd0ca7717979a2639
Amir Tamimi <amir.tamim@trojans.dsu.edu>
2019-09-05 20:15:35 -0700

    filter and clear functions

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ loadEventListeners();
 
 function loadEventListeners() {
   form.addEventListener('submit', addTask);
+  tasksList.addEventListener('click', removeTask);
+  clearBtn.addEventListener('click', clearTasks);
+  filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e) {
@@ -26,7 +29,49 @@ function addTask(e) {
   link.innerHTML = '<i class = "fa fa-remove"></i>';
   li.appendChild(link);
   tasksList.appendChild(li);
+
+  storeTaskInLocalStorage(taskInput.value);
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  }else{
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+function clearTasks(e) {
+  while(tasksList.firstChild){
+    tasksList.removeChild(tasksList.firstChild);
+  }
+}
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach
+  (function(task){
+      const item = task.firstChild.textContent;
+      if(item.toLowerCase().indexOf(text) != -1){
+        task.style.display = 'block';
+      }else{
+        task.style.display = 'none';
+      }
+    }
+  );
 }
